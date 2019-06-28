86586005017a4b5f13e12e0f466927c37fd220c3
Ryan Riginding <ryan@riginding.com>
2019-06-28 23:33:45 +0200

    feat: use nalgebra types

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,68 @@
+#[macro_use]
+extern crate approx;
+
+use nalgebra;
+
 fn main() {
-    println!("{:?}", Point::from((1.0, 2.0, 3.0)));
-    println!("{:?}", Vector::from((1.0, 2.0, 3.0)));
 }
 
-#[derive(Debug)]
-struct Point {
-    x: f32,
-    y: f32,
-    z: f32,
-}
+type Point = nalgebra::Point3<f32>;
+type Vector = nalgebra::Vector3<f32>;
 
-impl From<(f32, f32, f32)> for Point {
-    fn from(value: (f32, f32, f32)) -> Self {
-        Point {
-            x: value.0,
-            y: value.1,
-            z: value.2,
-        }
+impl Point {
+    fn neg(self) -> Point {
+        let origin = Point::new(0.0,0.0,0.0);
+        return origin - self
     }
 }
 
-impl PartialEq for Point {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y && self.z == other.z
-    }
+fn point(a: f32,b:f32,c:f32) -> Point {
+    return Point::new(a,b,c)
 }
 
-#[derive(Debug)]
-struct Vector {
-    x: f32,
-    y: f32,
-    z: f32,
-}
-
-impl From<(f32, f32, f32)> for Vector {
-    fn from(value: (f32, f32, f32)) -> Self {
-        Vector {
-            x: value.0,
-            y: value.1,
-            z: value.2,
-        }
-    }
-}
-
-impl PartialEq for Vector {
-    fn eq(&self, other: &Self) -> bool {
-        self.x == other.x && self.y == other.y && self.z == other.z
-    }
+fn vector(a: f32,b:f32,c:f32) -> Vector {
+    return Vector::new(a,b,c)
 }
 
 #[cfg(test)]
 mod tests {
-    // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
     #[test]
-    fn create_point_from_tuple() {
+    fn create_a_point() {
         assert_eq!(
-            Point::from((1.0, 2.0, 3.0)),
-            Point {
-                x: 1.0,
-                y: 2.0,
-                z: 3.0
-            }
+            point(1.0,2.0,3.0),
+            Point::new(1.0,2.0,3.0)
         );
     }
 
     #[test]
-    fn create_vector_from_tuple() {
+    fn create_a_vector() {
         assert_eq!(
-            Vector::from((1.0, 2.0, 3.0)),
-            Vector {
-                x: 1.0,
-                y: 2.0,
-                z: 3.0
-            }
+            vector(1.0,2.0,3.0),
+            Vector::new(1.0,2.0,3.0)
         );
+    }
+
+    #[test]
+    fn subtract_two_points() {
+        let p1 = point(1.1,2.2,3.3);
+        let p2 = point(1.1,1.1,1.1);
+        assert_relative_eq!(p1-p2, vector(0.0,1.1,2.2))
+    }
+
+    #[test]
+    fn subtract_vector_from_point() {
+        let p = point(1.1,2.2,3.3);
+        let v = vector(1.1,1.1,1.1);
+
+        assert_relative_eq!(p-v, point(0.0,1.1,2.2))
+    }
+
+    #[test]
+    fn subtract_two_vectors() {
+        let v1 = vector(1.1,2.2,3.3);
+        let v2 = vector(1.1,0.0,1.1);
+        assert_relative_eq!(v1-v2, vector(0.0,2.2,2.2))
     }
 }
